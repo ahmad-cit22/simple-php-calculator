2c8a223498722017f4855a60c39e8b0b67a9eafb
Nafis Ahmed <102437433+ahmad-cit22@users.noreply.github.com>
2022-05-24 11:35:30 +0600

    added

---
+++ calc_post.php
@@ -0,0 +1,40 @@
+<!-- <?php
+
+// if ($_POST["one"]) {
+//     if ($_POST["two"]) {
+//         if ($_POST["three"]) {
+
+//             if (isset($_POST["add"])) {
+//                 $addResult = $_POST["one"] + $_POST["two"] + $_POST["three"];
+//                 print_r("Addition result is : " . $addResult);
+//             } else if (isset($_POST["sub"])) {
+//                 $subResult = $_POST["one"] - $_POST["two"] - $_POST["three"];
+//                 print_r("Subtraction result is : " . $subResult);
+//             } else if (isset($_POST["multi"])) {
+//                 $multiResult = $_POST["one"] * $_POST["two"] * $_POST["three"];
+//                 print_r("Multiplication result is : " . $multiResult);
+//             } else if (isset($_POST["divi"])) {
+//                 $diviResult = $_POST["one"] / $_POST["two"] / $_POST["three"];
+//                 print_r("Division result is : " . $diviResult);
+//             }
+//         } else {
+//             echo "Number three is left blank";
+//         }
+//     } else {
+//         echo "Number two is left blank";
+//     }
+// } else {
+//     echo "Number one is left blank";
+// }
+
+
+
+
+// else if (isset($_POST["divi"])) {
+//                 $diviResult = $_POST["one"] / $_POST["two"] / $_POST["three"];
+//                 if ($_POST["two"] === 0 && $_POST["three"] === 0) {
+//                     echo "Infinity!";
+//                 } else {
+//                     print_r("Division result is : " . $diviResult);
+//                 }
+//             } -->
